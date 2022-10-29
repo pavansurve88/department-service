0e504f0405a1d4733e21188d933318ce939b870c
Pavan Surve <pavansurve88@gmail.com>
2022-10-29 18:29:23 +0530

    Initial Commit

---
+++ src/main/java/com/pavan/department/repository/DepartmentRepository.java
@@ -0,0 +1,11 @@
+package com.pavan.department.repository;
+
+import com.pavan.department.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+    Department findByDepartmentId(Long departmentId);
+
+}
